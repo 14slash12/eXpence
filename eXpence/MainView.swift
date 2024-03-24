68b0a67647253941fd0e9e71472e59d069897bc1
David Lin <davidlin1412@yahoo.de>
2024-03-24 19:01:15 +0100

    Adding SettingsView

--- eXpence/MainView.swift
+++ eXpence/MainView.swift
@@ -360,33 +360,368 @@ struct DateAdjustView: View {
 }
 
 struct SettingsView: View {
+    @Environment(\.modelContext) var modelContext
+    @Environment(\.colorScheme) var colorScheme
     @Environment(\.dismiss) var dismiss
+    @State var start: Date = Date()
+    @State var end: Date = Date()
 
     var body: some View {
-        VStack {
-            HStack {
-                Spacer()
-                Button {
-                    dismiss()
-                } label: {
-                    ZStack {
-                        Circle()
-                            .foregroundStyle(.ultraThinMaterial)
-                            .frame(width: .myLarge1)
-                        Image(systemName: "xmark")
-                            .foregroundStyle(.gray)
+        ScrollView {
+            VStack {
+                HStack {
+                    Spacer()
+                    Button {
+                        dismiss()
+                    } label: {
+                        ZStack {
+                            Circle()
+                                .foregroundStyle(.ultraThinMaterial)
+                                .frame(width: .myLarge1)
+                            Image(systemName: "xmark")
+                                .foregroundStyle(.gray)
+                        }
                     }
+
+                }
+                HStack {
+                    Text("Settings")
+                        .font(.title)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .padding([.top, .bottom])
+                    Spacer()
                 }
 
+                VStack {
+                    HStack {
+                        Text("Pro Features")
+                            .fontDesign(.monospaced)
+                            .font(.custom("Menlo", size: 21))
+                            .font(.title2)
+                            .fontWeight(.bold)
+                        Image(systemName: "wand.and.stars")
+                            .foregroundStyle(.purple)
+                        Spacer()
+                    }
+                    HStack {
+                        Text("Become a Pro member and get awesome features")
+                        Spacer()
+                    }
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Image(systemName: "appwindow.swipe.rectangle")
+                                .foregroundStyle(.black)
+                                .padding()
+                                .background {
+                                    RoundedRectangle(cornerRadius: .myCornerRadius)
+                                        .foregroundStyle(Color(.primary))
+                                }
+                            Text("Fantastic Home Screen Widgets")
+                        }
+
+                        HStack {
+                            Image(systemName: "list.number")
+                                .foregroundStyle(.black)
+                                .padding()
+                                .background {
+                                    RoundedRectangle(cornerRadius: .myCornerRadius)
+                                        .foregroundStyle(Color(.primary))
+                                }
+                            Text("Exports to CSV files")
+                        }
+
+                        HStack {
+                            Image(systemName: "wand.and.stars")
+                                .foregroundStyle(.black)
+                                .padding()
+                                .background {
+                                    RoundedRectangle(cornerRadius: .myCornerRadius)
+                                        .foregroundStyle(Color(.primary))
+                                }
+                            Text("Lifetime access")
+                        }
+
+                        HStack {
+                            Image(systemName: "heart.fill")
+                                .foregroundStyle(.black)
+                                .padding()
+                                .background {
+                                    RoundedRectangle(cornerRadius: .myCornerRadius)
+                                        .foregroundStyle(Color(.primary))
+                                }
+                            Text("Support solo project for even more future pro features. Thank you!")
+                                .frame(height: .myLarge1)
+
+                        }
+                    }
+
+                    Button {
+
+                    } label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: .myCornerRadius)
+                                .foregroundStyle(Color(.primary))
+                            Text("Become Pro")
+                                .foregroundStyle(.black)
+                                .fontDesign(.monospaced)
+                                .padding()
+                        }
+                    }
+                    .shadow(color: .purple, radius: 10)
+                    .padding([.leading, .trailing, .top])
+
+                    Button("Restore Purchase") {
+
+                    }
+                    .foregroundStyle(Color(.lightGray))
+                }
+                .padding()
+                .foregroundStyle(colorScheme == .light ? .white : .black)
+                .background {
+                    RoundedRectangle(cornerRadius: .myCornerRadius)
+                        .shadow(color: .purple, radius: 10)
+                }
+
+                Divider()
+
+                // Export
+
+                HStack {
+                    Text("Export")
+                        .fontDesign(.monospaced)
+                        .font(.custom("Menlo", size: 21))
+                        .font(.title2)
+                        .fontWeight(.bold)
+                    Spacer()
+                }
+
+
+                VStack {
+                    HStack {
+                        VStack {
+                            DatePicker(selection: $start) {
+                                HStack {
+                                    Image(systemName: "calendar")
+                                    Text("From")
+                                }
+                            }
+                            DatePicker(selection: $end) {
+                                HStack {
+                                    Image(systemName: "calendar")
+                                    Text("Until")
+                                }
+                            }
+                        }
+                        .padding([.trailing])
+
+                        ShareLink(item: generateURL() ?? URL(fileURLWithPath: "")) {
+                            Image(systemName: "square.and.arrow.up")
+                                .foregroundStyle(colorScheme == .light ? .white : .black)
+                        }
+                    }
+                    .padding()
+                    .background {
+                        GeometryReader { geo in
+                            RoundedRectangle(cornerRadius: .myCornerRadius)
+                                .foregroundStyle(Color(.primary))
+                                .opacity(0.75)
+
+                            Rectangle()
+                                .foregroundStyle(colorScheme == .light ? .black : .white)
+                                .offset(x: geo.size.width - .myLarge1)
+                                .clipShape(
+                                    .rect(
+                                        topLeadingRadius: 0,
+                                        bottomLeadingRadius: 0,
+                                        bottomTrailingRadius: .myCornerRadius,
+                                        topTrailingRadius: .myCornerRadius
+                                    )
+                                )
+                        }
+                    }
+                    .padding([.bottom])
+                    .shadow(radius: 5)
+
+                    HStack {
+                        Button {
+                            withAnimation {
+                                guard let dates = filterDates(from: Date(), by: .week) else { return }
+                                start = dates.start
+                                end = dates.end
+                            }
+                        } label: {
+                            HStack {
+                                Text("This week")
+                                    .foregroundStyle(.black)
+                                    .padding()
+                                Image(systemName: "arrow.right")
+                                    .foregroundStyle(.white)
+                                    .padding([.trailing])
+                            }
+                            .background {
+                                GeometryReader { geo in
+                                    RoundedRectangle(cornerRadius: .myCornerRadius)
+                                        .foregroundStyle(Color(.primary))
+                                        .opacity(0.75)
+
+                                    Rectangle()
+                                        .foregroundStyle(colorScheme == .light ? .black : .white)
+                                        .offset(x: geo.size.width - .myLarge1)
+                                        .clipShape(
+                                            .rect(
+                                                topLeadingRadius: 0,
+                                                bottomLeadingRadius: 0,
+                                                bottomTrailingRadius: .myCornerRadius,
+                                                topTrailingRadius: .myCornerRadius
+                                            )
+                                        )
+                                }
+                            }
+                        }
+                        .shadow(radius: 5)
+
+                        Button {
+                            withAnimation {
+                                guard let dates = filterDates(from: Date(), by: .month) else { return }
+                                start = dates.start
+                                end = dates.end
+                            }
+                        } label: {
+                            HStack {
+                                Text("This month")
+                                    .foregroundStyle(.black)
+                                    .padding()
+                                Image(systemName: "arrow.right")
+                                    .foregroundStyle(.white)
+                                    .padding([.trailing])
+                            }
+                            .background {
+                                GeometryReader { geo in
+                                    RoundedRectangle(cornerRadius: .myCornerRadius)
+                                        .foregroundStyle(Color(.primary))
+                                        .opacity(0.75)
+
+                                    Rectangle()
+                                        .foregroundStyle(colorScheme == .light ? .black : .white)
+                                        .offset(x: geo.size.width - .myLarge1)
+                                        .clipShape(
+                                            .rect(
+                                                topLeadingRadius: 0,
+                                                bottomLeadingRadius: 0,
+                                                bottomTrailingRadius: .myCornerRadius,
+                                                topTrailingRadius: .myCornerRadius
+                                            )
+                                        )
+                                }
+                            }
+                        }
+                        .shadow(radius: 5)
+                    }
+                }
+                .padding([.bottom])
             }
-            Text("Settings")
-            Text("Current Status: Free")
-            Button("Restore Purchase") {
-                // Restore
+            .padding()
+            .onAppear {
+                datesToThisWeek()
             }
-            Spacer()
         }
-        .padding()
+    }
+
+    private func datesToThisWeek() {
+        guard let dates = filterDates(from: Date(), by: .week) else { return }
+        self.start = dates.start
+        self.end = dates.end
+    }
+
+    private func generateURL() -> URL? {
+        guard let predicate = filterPredicate(start: start, end: end) else { return nil }
+        let expenses = fetch(for: predicate)
+        return generateCSV(for: expenses)
+    }
+
+    private func fetch(for predicate: Predicate<Expense>) -> [Expense] {
+        let descriptor = FetchDescriptor<Expense>(predicate: predicate)
+
+        do {
+            return try modelContext.fetch(descriptor)
+        } catch {
+            print("ERROR")
+        }
+
+        return []
+    }
+
+    private func generateCSV(for expenses: [Expense]) -> URL {
+        var fileURL: URL!
+        // heading of CSV file.
+        let heading = "Name, Date, Amount, Category\n"
+        // file rows
+        let rows = expenses.map { "\($0.name),\($0.timestamp),\($0.amount), \($0.category?.name ?? "n.a.")" }
+
+        // rows to string data
+        let stringData = heading + rows.joined(separator: "\n")
+
+        do {
+
+            let path = try FileManager.default.url(for: .documentDirectory,
+                                                   in: .allDomainsMask,
+                                                   appropriateFor: nil,
+                                                   create: false)
+
+            fileURL = path.appendingPathComponent("eXpence-Report-\(Date()).csv")
+
+            // append string data to file
+            try stringData.write(to: fileURL, atomically: true , encoding: .utf8)
+            print(fileURL!)
+
+        } catch {
+            print("error generating csv file")
+        }
+        return fileURL
+    }
+
+    private func filterPredicate(from input: Date, by type: DateType) -> Predicate<Expense>? {
+        let dates = filterDates(from: input, by: type)
+        guard let start = dates?.start, let end = dates?.end else { return nil }
+
+        return filterPredicate(start: start, end: end)
+    }
+
+    private func filterPredicate(start: Date, end: Date) -> Predicate<Expense>? {
+        return #Predicate<Expense> { expense in
+            return expense.timestamp < end && expense.timestamp >= start
+        }
+    }
+
+    private func filterDates(from input: Date, by type: DateType) -> (start: Date, end: Date)? {
+        var start: Date
+        var end: Date
+
+        switch type {
+        case .day:
+            start = Calendar.current.startOfDay(for: input)
+            guard let endOfDay = Calendar.current.date(byAdding: .day, value: 1, to: start) else {
+                return nil
+            }
+            end = endOfDay
+
+        case .week:
+            guard let startOfWeek = input.startOfWeek, let endOfWeek = input.endOfWeek else {
+                return nil
+            }
+
+            start = startOfWeek
+            end = endOfWeek
+
+            print(start)
+            print(end)
+
+        case .month:
+            start = input.startOfMonth
+            end = input.endOfMonth
+        }
+        
+        return (start, end)
     }
 }
 
@@ -694,7 +1029,7 @@ struct MainView: View {
     @State var showAddView: Bool = false
     @State var showDataView: Bool = true
     @FocusState var isNameFocused:Bool
-    @State var showSettings: Bool = false
+    @State var showSettings: Bool = true
 
     enum Page {
         case list
